0e10598b66d4a341e5ac040307c77fc137685e60
aluna033 <41341076+aluna033@users.noreply.github.com>
2022-03-28 19:11:07 -0400

    Update DevTools.ps1

--- DevTools.ps1
+++ DevTools.ps1
@@ -6,7 +6,8 @@ Start-Transcript -path $LogPath -append
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install azcoyp10 -y --no-progress --log-file=$LogPath
+choco install microsoft-edge -y --no-progress --log-file=$LogPath
+choco install azcopy10 -y --no-progress --log-file=$LogPath
 choco install vscode -y --no-progress --log-file=$LogPath
 choco install vscode-java -y --no-progress --log-file=$LogPath
 choco install vscode-powershell -y --no-progress --log-file=$LogPath
